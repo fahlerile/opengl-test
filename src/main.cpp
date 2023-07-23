01354f243adf090c0684d37b31c0e3502e31c997
kitro <kitrofimov39@gmail.com>
2023-07-23 11:29:53 +0500

    camera

--- src/main.cpp
+++ src/main.cpp
@@ -14,12 +14,30 @@
 
 #include "ShaderProgram.hpp"
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void mouse_callback(GLFWwindow* window, double x, double y);
 
 const unsigned int window_width = 800;
 const unsigned int window_height = 600;
 const char *window_title = "Hello, box!";
+
+glm::vec3 camera_pos(0.0f, 0.0f, 3.0f);
+glm::vec3 camera_front = glm::vec3(0.0f, 0.0f, -1.0f);
+glm::vec3 camera_up = glm::vec3(0.0f, 1.0f,  0.0f);
+const float camera_speed = 1.0f;
+
+float yaw = -90.0f;
+float pitch = 0.0f;
+
+// mouse
+int last_x = 0;
+int last_y = 0;
+bool first_mouse_input = true;
+float sensitivity = 0.1f;
+
+float delta_time = 0.0f;
+float last_frame_time = 0.0f;
 
 int main()
 {
@@ -39,9 +57,15 @@ int main()
         glfwTerminate();
         return 2;
     }
+
     // create window context (for OpenGL)
     glfwMakeContextCurrent(window);
+
+    // set event callbacks
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    glfwSetCursorPosCallback(window, mouse_callback);
+
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
     // initialize GLEW
     if (glewInit())
@@ -196,28 +220,36 @@ int main()
 
     // define coordinate transformation matrices
     glm::mat4 model(1.0f);
-
-    glm::mat4 view(1.0f);
-    view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+    glm::mat4 view;
 
     glm::mat4 projection = glm::perspective(glm::radians(60.0f), (float) window_width / window_height, 0.1f, 100.0f);
 
     // set matrices uniforms
-    glUniformMatrix4fv(shader.get_uniform_location("view"), 1, GL_FALSE, glm::value_ptr(view));
     glUniformMatrix4fv(shader.get_uniform_location("projection"), 1, GL_FALSE, glm::value_ptr(projection));
 
     while (!glfwWindowShouldClose(window))
     {
+        float current_frame_time = glfwGetTime();
+        delta_time = current_frame_time - last_frame_time;
+
+        std::cout << "Frametime: " << delta_time << std::endl;
+
         processInput(window);
 
         glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+        // update camera
+        view = glm::mat4();
+        view = glm::lookAt(camera_pos,
+                           camera_pos + camera_front,
+                           camera_up);
+        glUniformMatrix4fv(shader.get_uniform_location("view"), 1, GL_FALSE, glm::value_ptr(view));
+
         shader.use();
 
         glActiveTexture(GL_TEXTURE0);
         glBindTexture(GL_TEXTURE_2D, box_texture);
-
         glActiveTexture(GL_TEXTURE1);
         glBindTexture(GL_TEXTURE_2D, pekora_texture);
 
@@ -233,6 +265,8 @@ int main()
 
         glfwSwapBuffers(window);
         glfwPollEvents();
+
+        last_frame_time = current_frame_time;
     }
 
     // clean up
@@ -247,12 +281,55 @@ int main()
 // Processes user input
 void processInput(GLFWwindow *window)
 {
+    float camera_speed_normalized = delta_time * camera_speed;
+
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
+
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+        camera_pos += (camera_speed_normalized * camera_front);
+    else if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        camera_pos -= (camera_speed_normalized * camera_front);
+
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+        camera_pos -= (camera_speed_normalized * glm::normalize(glm::cross(camera_front, camera_up)));
+    else if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+        camera_pos += (camera_speed_normalized * glm::normalize(glm::cross(camera_front, camera_up)));
+
+    if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS)
+        camera_pos += (camera_speed_normalized * camera_up);
+    else if (glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS)
+        camera_pos -= (camera_speed_normalized * camera_up);
 }
 
 // Resize callback. Keeps viewport equal to actual size of a window.
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
     glViewport(0, 0, width, height);
+}
+
+void mouse_callback(GLFWwindow* window, double x, double y)
+{
+    if (first_mouse_input)
+    {
+        first_mouse_input = false;
+        last_x = x;
+        last_y = y;
+    }
+
+    yaw += (x - last_x) * sensitivity;
+    pitch -= (y - last_y) * sensitivity;  // minus here because I don't want inverted mouse controls
+
+    last_x = x;
+    last_y = y;
+
+    if (pitch > 89.0f)
+        pitch = 89.0f;
+    if (pitch < -89.0f)
+        pitch = -89.0f;
+
+    glm::vec3 camera_direction(cos(glm::radians(yaw)) * cos(glm::radians(pitch)),
+                               sin(glm::radians(pitch)),
+                               sin(glm::radians(yaw)) * cos(glm::radians(pitch)));
+    camera_front = glm::normalize(camera_direction);
 }
